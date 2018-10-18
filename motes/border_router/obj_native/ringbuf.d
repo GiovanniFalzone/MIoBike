1e27f131263c71caf4b8158eecc8c89117af5bf0
GiovanniFalzone <h.hunt3r@gmail.com>
2018-10-18 12:36:55 +0200

    Working Border Router with Deveices discovery

---
+++ motes/border_router/obj_native/ringbuf.d
@@ -0,0 +1,8 @@
+obj_native/ringbuf.o: /home/osboxes/contiki/core/lib/ringbuf.c \
+ /home/osboxes/contiki/core/./lib/ringbuf.h \
+ /home/osboxes/contiki/platform/native/./contiki-conf.h \
+ /home/osboxes/contiki/platform/native/./ctk/ctk-curses.h project-conf.h
+/home/osboxes/contiki/core/lib/ringbuf.c :
+ /home/osboxes/contiki/core/./lib/ringbuf.h :
+ /home/osboxes/contiki/platform/native/./contiki-conf.h :
+ /home/osboxes/contiki/platform/native/./ctk/ctk-curses.h project-conf.h :
