72e8729e6fcb0dff8c8bab91caff026c66008d1e
GiovanniFalzone <h.hunt3r@gmail.com>
2018-11-07 10:24:04 +0100

    common libraries used by MN and IN

---
+++ MIoBike_Utils/src/main/java/it/unipi/iot/MIoBike/MIoBike_Utils/CoAPMonitor.java
@@ -0,0 +1,50 @@
+package it.unipi.iot.MIoBike.MIoBike_Utils;
+
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.eclipse.californium.core.server.resources.Resource;
+import it.unipi.iot.MIoBike.MIoBike_MN_ADN.*;
+
+public class CoAPMonitor extends CoapServer {
+	private static final int COAP_PORT = 5685;
+	private static String NotificationManager_name;
+	void addEndpoints() {
+		for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+			if (((addr instanceof Inet4Address)) || (addr.isLoopbackAddress()))
+			{
+				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+				addEndpoint(new CoapEndpoint(bindToAddress));
+			}
+		}
+	}
+	
+	public CoAPMonitor(String NotificationManager_name) throws SocketException {
+		this.NotificationManager_name = NotificationManager_name;
+		add(new Resource[] { new Monitor() });
+	}
+	
+	class Monitor extends CoapResource {
+		public Monitor() {
+			super(NotificationManager_name);	
+			getAttributes().setTitle(NotificationManager_name);
+		}
+		 
+		public void handlePOST(CoapExchange exchange) {
+			exchange.respond(ResponseCode.CREATED);
+			byte[] content = exchange.getRequestPayload();
+			String contentStr = new String(content);
+			Bike_Manager.handle_Notification(contentStr);
+			System.out.println("---------------Notification Handler---------------");
+			System.out.println(contentStr);
+			System.out.println("--------------------------------------------------");
+		}
+	}
+}
