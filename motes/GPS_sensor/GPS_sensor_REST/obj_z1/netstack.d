944c4c564c1a633e4c166705b7300c1860536fdc
GiovanniFalzone <h.hunt3r@gmail.com>
2018-11-07 09:33:54 +0100

    first REST implementation of the GPS sensor

---
+++ motes/GPS_sensor/GPS_sensor_REST/obj_z1/netstack.d
@@ -0,0 +1,20 @@
+obj_z1/netstack.o: /home/osboxes/contiki/core/net/netstack.c \
+ /home/osboxes/contiki/core/./net/netstack.h \
+ /home/osboxes/contiki/platform/z1/./contiki-conf.h \
+ /home/osboxes/contiki/platform/z1/./platform-conf.h \
+ /home/osboxes/contiki/cpu/msp430/./msp430def.h project-conf.h \
+ /home/osboxes/contiki/core/./net/llsec/llsec.h \
+ /home/osboxes/contiki/core/./net/mac/mac.h \
+ /home/osboxes/contiki/core/./dev/radio.h \
+ /home/osboxes/contiki/core/./net/mac/rdc.h \
+ /home/osboxes/contiki/core/./net/mac/framer.h
+/home/osboxes/contiki/core/net/netstack.c :
+ /home/osboxes/contiki/core/./net/netstack.h :
+ /home/osboxes/contiki/platform/z1/./contiki-conf.h :
+ /home/osboxes/contiki/platform/z1/./platform-conf.h :
+ /home/osboxes/contiki/cpu/msp430/./msp430def.h project-conf.h :
+ /home/osboxes/contiki/core/./net/llsec/llsec.h :
+ /home/osboxes/contiki/core/./net/mac/mac.h :
+ /home/osboxes/contiki/core/./dev/radio.h :
+ /home/osboxes/contiki/core/./net/mac/rdc.h :
+ /home/osboxes/contiki/core/./net/mac/framer.h :
---
+++ motes/GPS_sensor/GPS_sensor_UDP_Java/obj_native/netstack.d
@@ -0,0 +1,18 @@
+obj_native/netstack.o: /home/osboxes/contiki/core/net/netstack.c \
+ /home/osboxes/contiki/core/./net/netstack.h \
+ /home/osboxes/contiki/platform/native/./contiki-conf.h \
+ /home/osboxes/contiki/platform/native/./ctk/ctk-curses.h \
+ /home/osboxes/contiki/core/./net/llsec/llsec.h \
+ /home/osboxes/contiki/core/./net/mac/mac.h \
+ /home/osboxes/contiki/core/./dev/radio.h \
+ /home/osboxes/contiki/core/./net/mac/rdc.h \
+ /home/osboxes/contiki/core/./net/mac/framer.h
+/home/osboxes/contiki/core/net/netstack.c :
+ /home/osboxes/contiki/core/./net/netstack.h :
+ /home/osboxes/contiki/platform/native/./contiki-conf.h :
+ /home/osboxes/contiki/platform/native/./ctk/ctk-curses.h :
+ /home/osboxes/contiki/core/./net/llsec/llsec.h :
+ /home/osboxes/contiki/core/./net/mac/mac.h :
+ /home/osboxes/contiki/core/./dev/radio.h :
+ /home/osboxes/contiki/core/./net/mac/rdc.h :
+ /home/osboxes/contiki/core/./net/mac/framer.h :
