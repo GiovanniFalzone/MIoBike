f107d785040815ebdf5f00cf1bce990dcebca903
GiovanniFalzone <h.hunt3r@gmail.com>
2018-11-07 10:22:15 +0100

    first implementation of the IN Application, it's a maven project

---
+++ MIoBike_IN_ADN/src/main/java/it/unipi/iot/MIoBike/MIoBike_IN_ADN/App.java
@@ -0,0 +1,13 @@
+package it.unipi.iot.MIoBike.MIoBike_IN_ADN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
